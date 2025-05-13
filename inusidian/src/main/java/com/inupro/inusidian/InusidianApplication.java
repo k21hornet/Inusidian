71868b282cb7882929a1cc38bf8820b85b5146fa
k21hornet <knt0535.tk@gmail.com>
2025-05-13 20:43:04 +0900

    add: Spring Boot Project

---
+++ inusidian/src/main/java/com/inupro/inusidian/InusidianApplication.java
@@ -0,0 +1,13 @@
+package com.inupro.inusidian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InusidianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InusidianApplication.class, args);
+	}
+
+}
