213d4b9c429d8b1eb2685c011b8d0523dda5e179
k21hornet <knt0535.tk@gmail.com>
2026-08-27 08:29:11 +0900

    feat: bg, header

--- sniff/Inusidian/App/HomeView.swift
+++ sniff/Inusidian/App/HomeView.swift
@@ -7,32 +7,55 @@ import SwiftUI
 
 struct HomeView: View {
     @ObservedObject var authService: AuthenticationService
-
+    
     var body: some View {
-        if authService.isLoading {
-            ProgressView("ローディング中")
-        } else if authService.isAuthenticated {
-            VStack {
-                Text("チワワわしゃわしゃ")
-                Button{
-                    Task {
-                        await authService.logout()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            ScrollView {
+                if authService.isLoading {
+                    ProgressView("ローディング中")
+                } else if authService.isAuthenticated {
+                    VStack {
+                        HStack {
+                            Image("inusidian")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 160)
+                            Spacer()
+                            Button{
+                                Task {
+                                    await authService.logout()
+                                }
+                            } label: {
+                                Image("logout")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 24)
+                            }
+                        }
+                        .padding(.bottom)
+                        
+                        Text("チワワわしゃわしゃ")
+                        Spacer()
                     }
-                } label: {
-                    Text("ログアウト")
-                }
-            }
-        } else {
-            VStack {
-                Button{
-                    Task {
-                        await authService.login()
+                    .padding()
+                } else {
+                    VStack {
+                        Button{
+                            Task {
+                                await authService.login()
+                            }
+                        } label: {
+                            Text("わしゃわしゃする")
+                        }
                     }
-                } label: {
-                    Text("わしゃわしゃする")
                 }
             }
         }
+        
     }
 }
 
